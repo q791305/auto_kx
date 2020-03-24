b4a91c0849bbfb001a53ac3242deedb0a1eee8d2
new1 <48582734+q791305@users.noreply.github.com>
2020-03-24 21:26:33 +0800

    Update auto_selenium.py

--- auto_selenium.py
+++ auto_selenium.py
@@ -1,10 +1,15 @@
-from selenium.webdriver import Chrome #設定用python控制網頁的函式庫
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = Chrome("./chromedriver")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome("/root/chromedriver", options=chrome_options)
+
 driver.get("https://eip.kx.com.tw/kxeip/") #先進入擎昊網頁
-driver.find_element_by_name("txtAccount").send_keys("")
-driver.find_element_by_name("txtPassword").send_keys("")
+driver.find_element_by_name("txtAccount").send_keys("eric.yang")
+driver.find_element_by_name("txtPassword").send_keys("KX#08564.")
 driver.find_element_by_id("btnLogin").click()
 
 driver.get("https://eip.kx.com.tw/KxEIP/Modules/Personal/PersonalWorkLog")
@@ -13,4 +18,5 @@ driver.find_element_by_class_name("btnBatchFix").click()
 
 time.sleep(3)  #停止3秒
 driver.close()  #關閉網頁
+
 
