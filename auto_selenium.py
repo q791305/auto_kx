df53e7efee75dec274f337a325332f777d0479c7
new1 <48582734+q791305@users.noreply.github.com>
2020-03-25 10:10:25 +0800

    Update auto_selenium.py

--- auto_selenium.py
+++ auto_selenium.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome("/root/chromedriver", options=chrome_options)
 
+account=os.popen('cat /root/file1').read().replace("\n", "")
+key=os.popen('cat /root/file2').read().replace("\n", "")
 driver.get("https://eip.kx.com.tw/kxeip/") #先進入擎昊網頁
-driver.find_element_by_name("txtAccount").send_keys("")
-driver.find_element_by_name("txtPassword").send_keys("")
+driver.find_element_by_name("txtAccount").send_keys(account)
+driver.find_element_by_name("txtPassword").send_keys(key)
 driver.find_element_by_id("btnLogin").click()
 
 driver.get("https://eip.kx.com.tw/KxEIP/Modules/Personal/PersonalWorkLog")
@@ -18,5 +21,4 @@ driver.find_element_by_class_name("btnBatchFix").click()
 
 time.sleep(3)  #停止3秒
 driver.close()  #關閉網頁
-
 
