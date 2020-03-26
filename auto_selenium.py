fa33de95314caf81d093eb4fde84e0c2265b8b26
new1 <48582734+q791305@users.noreply.github.com>
2020-03-27 00:36:15 +0800

    Update auto_selenium.py

--- auto_selenium.py
+++ auto_selenium.py
@@ -8,26 +8,26 @@ account=os.popen('cat /root/auto_python/.file1').read().replace("\n", "")
 key=os.popen('cat /root/auto_python/.file2').read().replace("\n", "")
 try:
 
-  chrome_options = Options()
-  chrome_options.add_argument("--headless")
-  chrome_options.add_argument("--no-sandbox")
-  driver = webdriver.Chrome("/root/auto_python/chromedriver", options=chrome_options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome("/root/auto_python/chromedriver", options=chrome_options)
   
-  driver.get("https://eip.kx.com.tw/kxeip/") #先進入擎昊網頁
-  driver.find_element_by_name("txtAccount").send_keys(account)
-  driver.find_element_by_name("txtPassword").send_keys(key)
-  driver.find_element_by_id("btnLogin").click()
+    driver.get("https://eip.kx.com.tw/kxeip/") #先進入擎昊網頁
+    driver.find_element_by_name("txtAccount").send_keys(account)
+    driver.find_element_by_name("txtPassword").send_keys(key)
+    driver.find_element_by_id("btnLogin").click()
 
-  driver.get("https://eip.kx.com.tw/KxEIP/Modules/Personal/PersonalWorkLog")
-  driver.find_element_by_class_name("btnBatchFix").click()
+    driver.get("https://eip.kx.com.tw/KxEIP/Modules/Personal/PersonalWorkLog")
+    driver.find_element_by_class_name("btnBatchFix").click()
 
 
-  time.sleep(3)  #停止3秒
-  driver.close()  #關閉網頁
+    time.sleep(3)  #停止3秒
+    driver.close()  #關閉網頁
 
 except:
-  os.system('echo "打卡處理失敗" | mailx -r "python_auto@admin" -S smtp="{0}" -s "mail_title" {1}@kx.com.tw'.format(mail,account))
+    os.system('echo "打卡處理失敗" | mailx -r "python_auto@admin" -S smtp="{0}" -s "mail_title" {1}@kx.com.tw'.format(mail,account))
   
 else:
-  os.system('echo "打卡成功" | mailx -r "[error]python_auto@admin" -S smtp="{0}" -s "mail_title" {1}@kx.com.tw'.format(mail,account))
+    os.system('echo "打卡成功" | mailx -r "[error]python_auto@admin" -S smtp="{0}" -s "mail_title" {1}@kx.com.tw'.format(mail,account))
   
